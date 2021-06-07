4f4dd1b3562a771d8514b899f26f8ac805238f98
Nicolas Brunie <nicolas.brunie@sifive.com>
2021-06-07 08:12:12 -0700

    test: adding Basic NaN/Inf conversion testing

---
+++ test.py
@@ -0,0 +1,33 @@
+from script_hardfloat_ieee import RecFNtoIEEE, RecFNtoIEEE_s2i
+
+def test_f32_nan():
+    recf32_nan = ((7 << 6) << 23)
+    ieeef32_nan = (255 << 23) | (1 << 22) | ((1 << 22) - 1)
+    assert RecFNtoIEEE(recf32_nan, size=32) == ieeef32_nan
+    assert RecFNtoIEEE_s2i(hex(recf32_nan), size=32) == ieeef32_nan
+
+def test_f64_nan():
+    recf64_nan = ((7 << 9) << 52)
+    ieeef64_nan = (2047 << 52) | (1 << 51) | ((1 << 51) - 1)
+    assert RecFNtoIEEE(recf64_nan, size=64) == ieeef64_nan
+    assert RecFNtoIEEE_s2i(hex(recf64_nan), size=64) == ieeef64_nan
+
+def test_f32_inf():
+    recf32_inf = ((6 << 6) << 23)
+    ieeef32_inf = (255 << 23)
+    assert RecFNtoIEEE(recf32_inf, size=32) == ieeef32_inf
+    assert RecFNtoIEEE_s2i(hex(recf32_inf), size=32) == ieeef32_inf
+
+def test_f64_inf():
+    recf64_inf = ((6 << 9) << 52)
+    ieeef64_inf = (2047 << 52)
+    assert RecFNtoIEEE(recf64_inf, size=64) == ieeef64_inf
+    assert RecFNtoIEEE_s2i(hex(recf64_inf), size=64) == ieeef64_inf
+
+if __name__ == "__main__":
+    # to run when no suitable verison of pytest is available
+    test_f32_nan()
+    test_f64_nan()
+
+    test_f32_inf()
+    test_f64_inf()
