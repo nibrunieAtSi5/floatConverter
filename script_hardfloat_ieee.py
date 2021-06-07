d754539b6ad89b9bb348039b7e1fd48968ff16fa
Nicolas Brunie <nicolas.brunie@sifive.com>
2021-06-07 08:11:56 -0700

    doc: improving comments and factorizing str conversion out of RecFNtoIEEE

--- script_hardfloat_ieee.py
+++ script_hardfloat_ieee.py
@@ -7,15 +7,19 @@ class IEEEFN:
         self.expSize = expSize
 
     def toHardFloatRecFN(self):
+        """ build the equivalent hardfloat recoded format """
         return HardFloatRecFN(self.sigSize, self.expSize + 1)
 
     def makeInf(self, sign):
+        """ build infinite value with sign """
         return ((sign << self.expSize) | (2**self.expSize - 1)) << (self.sigSize - 1)
 
     def makeNaN(self, sign=0, qbit=1):
+        """ build a NaN whose sign is sign, and whose quiet bit is set to qbit """
         return self.makeInf(sign) | (2**(self.sigSize-2) - 1) | (qbit << (self.sigSize - 2))
 
     def buildValue(self, sign, exp, sig):
+        """ built an arbitrary value """
         assert 0 <= sign <= 1
         assert 0 <= exp <= (2**self.expSize - 1)
         assert 0 <= sig <= (2**(self.sigSize - 1) - 1)
@@ -27,39 +31,57 @@ class HardFloatRecFN:
         self.expSize = expSize
 
     def toIEEEFN(self):
+        """ build the equivalent IEEE-754 floating-point encoding """
         return IEEEFN(self.sigSize, self.expSize-1)
 
     @property
     def minNormalExp(self):
+        """ return the format minimal exponent for a normal number """
         return 2**(self.expSize - 2) + 2
     @property
     def minSubNormalExp(self):
+        """ return the format minimal exponent for a subnormal number """
         return 2**(self.expSize - 2) + 2 - self.sigSize
 
     def expNormalToIEEE(self, expValue):
+        """ convert the exponent biased value to its IEEE biased encoding
+            assuming it is the exponent of a normal number """
         return expValue - 2**(self.expSize - 2) - 1
 
 
     def isExpInf(self, expValue):
+        """ check if encoded expValue is an infinity's exponent """
         return (expValue >> (self.expSize - 3)) == 0x6
     def isExpNaN(self, expValue):
+        """ check if encoded expValue is a NaN's exponent """
         return (expValue >> (self.expSize - 3)) == 0x7
     def isExpZero(self, expValue):
+        """ check if encoded expValue is a zero's exponent """
         return (expValue >> (self.expSize - 3)) == 0x0
 
+
+# Map of IEEE-754 standard floating-point formats
 IEEE_FORMAT_MAP = {
-        16: IEEEFN(11, 5),
-        32: IEEEFN(24, 8),
-        64: IEEEFN(53, 11),
+        16: IEEEFN(11, 5),  # half precision
+        32: IEEEFN(24, 8),  # single precision
+        64: IEEEFN(53, 11), # double precision
 }
+
+# Map of hardfloat's recoded floating-point formats
 HARDFLOAT_FORMAT_MAP = {s: IEEE_FORMAT_MAP[s].toHardFloatRecFN() for s in IEEE_FORMAT_MAP}
 
 def bitMask(size):
+    """ generate a ful set bit mask of width <size> """
     return 2**size - 1
 
-def RecFNtoIEEE(v, base=16, size=64):
+def RecFNtoIEEE_s2i(v, base=16, size=64):
+    """ convert a string-encoded value <v> from recoded to IEEE format """
     v = v.replace("_", "")
     v = int(v, base=base)
+    return RecFNtoIEEE(v, size=size)
+
+def RecFNtoIEEE(v, base=16, size=64):
+    """ convert a value <v> from recoded format to IEEE format """
     recfn = HARDFLOAT_FORMAT_MAP[size]
     SIGMASK = bitMask(recfn.sigSize - 1)
     sig = v & SIGMASK
@@ -95,7 +117,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     if args.command == "recfntoieee":
-        print(hex(RecFNtoIEEE(args.values[0], base=16, size=args.input_size))) 
+        print(hex(RecFNtoIEEE_s2i(args.values[0], base=16, size=args.input_size))) 
 
 
 
