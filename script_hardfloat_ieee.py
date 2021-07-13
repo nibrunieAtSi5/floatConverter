87cb7dd802db2c4a84e47487e2f3d33425debc11
Nicolas Brunie <nicolas.brunie@sifive.com>
2021-07-13 05:01:17 -0700

    adding support for ieee to recfn conversion

--- script_hardfloat_ieee.py
+++ script_hardfloat_ieee.py
@@ -1,10 +1,18 @@
 import argparse
+import random
 
 
 class IEEEFN:
     def __init__(self, sigSize, expSize):
         self.sigSize = sigSize
         self.expSize = expSize
+
+    @property
+    def expInfOrNaN(self):
+        return (2**self.expSize-1)
+    @property
+    def bias(self):
+        return -(2**(self.expSize - 1) - 1)
 
     def toHardFloatRecFN(self):
         """ build the equivalent hardfloat recoded format """
@@ -27,6 +35,13 @@ class IEEEFN:
         assert 0 <= exp <= (2**self.expSize - 1)
         assert 0 <= sig <= (2**(self.sigSize - 1) - 1)
         return (((sign << self.expSize) | exp) << self.sigSize - 1) | sig
+
+    def isExpInforNaN(self, expValue):
+        """ check if encoded expValue is an infinity's exponent """
+        return expValue == self.expInfOrNaN
+    def isExpZeroOrSubnormal(self, expValue):
+        """ check if encoded expValue is a zero's exponent """
+        return expValue == 0
 
 class HardFloatRecFN:
     def __init__(self, sigSize, expSize):
@@ -61,6 +76,23 @@ class HardFloatRecFN:
     def isExpZero(self, expValue):
         """ check if encoded expValue is a zero's exponent """
         return (expValue >> (self.expSize - 3)) == 0x0
+
+    def makeInf(self, sign, payload=0):
+        """ build infinite value with sign """
+        return (((sign << self.expSize) | (0x6 << (self.expSize - 3))) << (self.sigSize - 1)) | payload
+
+    def makeNaN(self, sign=0, qbit=1, payload=0):
+        """ build a NaN whose sign is sign, and whose quiet bit is set to qbit """
+        return (((sign << self.expSize) | \
+               (0x7 << (self.expSize - 3))) << (self.sigSize - 1)) | \
+               payload | (qbit << (self.sigSize - 2))
+
+    def buildValue(self, sign, exp, sig):
+        """ built an arbitrary value """
+        assert 0 <= sign <= 1
+        assert 0 <= exp <= (2**self.expSize - 1)
+        assert 0 <= sig <= (2**(self.sigSize - 1) - 1)
+        return (((sign << self.expSize) | exp) << self.sigSize - 1) | sig
 
 
 # Map of IEEE-754 standard floating-point formats
@@ -110,19 +142,77 @@ def RecFNtoIEEE(v, base=16, size=64):
     else:
         return ieeefn.buildValue(sign, recfn.expNormalToIEEE(exp),sig) 
 
+def lzc(value, size):
+    """ Leading Zero Count """
+    count = 0
+    index = size - 1
+    while (value >> index) == 0 and index >= 0:
+        count += 1
+        index -= 1
+    return count
+
+def IEEEtoRecFN(v, base=16, size=64, randomizePayload=True):
+    ieeeFmt = IEEE_FORMAT_MAP[size]
+    recfn = ieeeFmt.toHardFloatRecFN()
+
+    SIGMASK = bitMask(ieeeFmt.sigSize - 1)
+    sig = v & SIGMASK
+    EXPMASK = bitMask(ieeeFmt.expSize)
+    exp = (v >> (ieeeFmt.sigSize - 1)) & EXPMASK
+    sign = (v >> (ieeeFmt.sigSize - 1 + ieeeFmt.expSize)) & 0x1
+    if ieeeFmt.isExpInforNaN(exp):
+        if sig == 0:
+            # infinity
+            infPayload = random.randrange(2**(ieeeFmt.sigSize - 1)) if randomizePayload else 0
+            return recfn.makeInf(sign, payload=infPayload)
+        else:
+            # NaN
+            return recfn.makeNaN(sign, qbit=(sig >> (ieeeFmt.sigSize - 2)), payload=sig)
+    elif ieeeFmt.isExpZeroOrSubnormal(exp):
+        if sig == 0:
+            # zero
+            return recfn.buildValue(sign, 0, 0)
+        else:
+            # subnormal
+            normShift = lzc(sig, ieeeFmt.sigSize - 1)
+            # normalizing significand and removing implicit one
+            sig = (sig << (normShift + 1)) & SIGMASK
+            biasedExp = recfn.minNormalExp - 1 - normShift
+            return recfn.buildValue(sign, biasedExp, sig)
+    else:
+        # normal
+        biasedExp = (exp - 1) + recfn.minNormalExp
+        return recfn.buildValue(sign, biasedExp, sig)
+
+def IEEEToRecFN_s2i(v, base=16, size=64, randomizePayload=True):
+    """ convert a string-encoded value <v> from recoded to IEEE format """
+    v = v.replace("_", "")
+    v = int(v, base=base)
+    return IEEEtoRecFN(v, size=size, randomizePayload=randomizePayload)
+
+assert(lzc(0xff, 8) == 0)
+assert(lzc(0xf, 13) == 9)
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='hardfloat/IEEE converter')
-    parser.add_argument('command', type=str, choices=["recfntoieee"],
+    parser.add_argument('command', type=str, choices=["recfntoieee", "ieeetorecfn"],
                         help='command to be executed')
     parser.add_argument('values', metavar='V', type=str, nargs='+',
                         help='value')
     parser.add_argument('--input-size', type=int, action='store', default=64,
                         help='input value size')
+    parser.add_argument('--payload-to-zero', const=True, default=False, action='store_const', 
+                        help='force don\'t care RecFN payload to zero')
 
     args = parser.parse_args()
     if args.command == "recfntoieee":
         print(hex(RecFNtoIEEE_s2i(args.values[0], base=16, size=args.input_size))) 
+    elif args.command == "ieeetorecfn":
+        print(hex(IEEEToRecFN_s2i(args.values[0], base=16, size=args.input_size, randomizePayload=not args.payload_to_zero))) 
+    else:
+        raise NotImplementedError
 
 
 
